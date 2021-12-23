a22bfed4dfacd65482b02d621d7189aa3a99e000
Aleksey <lexa.shvetsoff@yandex.ru>
2021-12-23 21:26:26 +0800

    Добавлена функция валидации email

--- scripts.py
+++ scripts.py
@@ -1,4 +1,5 @@
 from random import randint
+import re
 
 # тесовый набор
 players_1 = ['Katy', 'John', 'Bill', 'Marry']  
@@ -17,8 +18,24 @@ def drawing_of_lots(players):
                 break
     return result
 
+
+def is_valid_email(email):
+    regex = re.compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+    # return re.fullmatch(regex, email)
+    if re.fullmatch(regex, email):
+        return True
+    else:
+        return False
+
+
 # тест
 for i in range(10):
     print(drawing_of_lots(players_1))
     print(drawing_of_lots(players_2))
     print(drawing_of_lots(players_3))
+
+
+print(is_valid_email("name.surname@gmail.com"))
+print(is_valid_email("anonymous123@yahoo.co.uk"))
+print(is_valid_email("anonymous123@...uk"))
+print(is_valid_email("...@domain.us"))
